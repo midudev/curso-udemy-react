0f6a96e28abae0a38358afc1da9ef72386cb2b4a
David G <david.garcia@scmspain.com>
2017-11-18 19:37:17 +0100

    adds refs example in componentDidMount

--- src/sections/live-cicle/componentDidMount.js
+++ src/sections/live-cicle/componentDidMount.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 
 class ChildComponent extends Component {
   componentDidMount () {
     console.log('El componente CHILD se ha montado en el DOM')
   }
 
+  getInputValue = () => ReactDOM.findDOMNode(this.refs.input).value
+
   render () {
     return (
-      <h3>Este es el componente Child</h3>
+      <input  style={{ padding: '10px', fontSize:'24px', width: '400px'}}
+              type='text' ref='input'
+              defaultValue='estoy en un child component'
+      />
     )
   }
 }
@@ -15,13 +21,16 @@ class ChildComponent extends Component {
 export default class ParentComponent extends Component {
   componentDidMount () {
     console.log('El componente PARENT se ha montado en el DOM')
+    
+    // Mostramos como podemos acceder a las refs una vez montado el elemento en el DOM
+    console.log(this.refs.childComponent.getInputValue())
   }
 
   render() {
     return (
-      <div>
+      <div className={'parentContainer'}>
         <h1>Este es el componente Parent</h1>
-        <ChildComponent />
+        <ChildComponent ref="childComponent" />
       </div>
     )
   }

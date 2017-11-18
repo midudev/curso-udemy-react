41c75bf49e4608a6c5614b851d5bd67af3bad677
Udemy <udemy@AFE0806015.local>
2017-11-18 17:32:55 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import FetchExample from './sections/fetch-example'
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <FetchExample />
+      </div>
+    );
+  }
+}
+
+export default App;
